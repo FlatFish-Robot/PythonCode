91369f9390e5e0d92a8396353cd30df9e5402a7b
Tim <tim.mockford@aceschools.net>
2017-02-19 22:20:15 +0000

    New LCD driver

--- core.py
+++ core.py
@@ -6,85 +6,13 @@ import os
 import hcsr04
 import piconzero as pz
 from gpiozero import Button
+import I2C_LCD_driver
 
 #__________________________________________________________________
-#Intialise the display - from http://www.raspberrypi-spy.co.uk/2015/05/using-an-i2c-enabled-lcd-screen-with-the-raspberry-pi/
-
-# Define some device parameters
-I2C_ADDR  = 0x3f # I2C device address
-LCD_WIDTH = 16   # Maximum characters per line
-
-# Define some device constants
-LCD_CHR = 1 # Mode - Sending data
-LCD_CMD = 0 # Mode - Sending command
-
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
-LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
-
-LCD_BACKLIGHT  = 0x08  # On
-#LCD_BACKLIGHT = 0x00  # Off
-
-ENABLE = 0b00000100 # Enable bit
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
-
-#Open I2C interface
-#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
-bus = smbus.SMBus(1) # Rev 2 Pi uses 1
-
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
-
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = the data
-  # mode = 1 for data
-  #        0 for command
-
-  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
-
-  # High bits
-  bus.write_byte(I2C_ADDR, bits_high)
-  lcd_toggle_enable(bits_high)
-
-  # Low bits
-  bus.write_byte(I2C_ADDR, bits_low)
-  lcd_toggle_enable(bits_low)
-
-def lcd_toggle_enable(bits):
-  # Toggle enable
-  time.sleep(E_DELAY)
-  bus.write_byte(I2C_ADDR, (bits | ENABLE))
-  time.sleep(E_PULSE)
-  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-  time.sleep(E_DELAY)
-
-def lcd_string(message,line):
-  # Send string to display
-
-  message = message.ljust(LCD_WIDTH," ")
-
-  lcd_byte(line, LCD_CMD)
-
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
-
-#__________________________________________________________________
-#initiate piconzero
+#initiate piconzero and display
 
 pz.init() #initiate hardware
+mylcd = I2C_LCD_driver.lcd() #assign LCD to variable for ease of use
 
 pz.setInputConfig(0,0) #developer switch is input 0 and digital
 pz.setInputConfig(1,0) #right IR sensor is input 1 and digital
@@ -109,12 +37,15 @@ RANGE = hcsr04.getDistance() #assign HC-SR04 range to variable
 
 def remotecontrol():
     print ("Remote Control Program Active")
-    lcd_string("Remote Control  <",LCD_LINE_1)
-    lcd_string("Select Ends     <",LCD_LINE_2)
+    mylcd.lcd_display_string("Remote Control", 1)
+    mylcd.lcd_display_string("Select Ends", 2)
+    time.sleep(2)
     RUN = 1
     while RUN == 1:
         x = joystick.axes[0].corrected_value()
         y = joystick.axes[1].corrected_value()
+        mylcd.lcd_display_string("X: %d %%" % x, 1)
+        mylcd.lcd_display_string("Y: %d %%" % y, 2)
         if buttons_pressed & 1 << SixAxis.BUTTON_SELECT:
             RUN = 0
         elif 0.1 >= x >= -0.1 and 0.1 >= y >= -0.1: #stop
@@ -122,70 +53,70 @@ def remotecontrol():
             y = abs(y)
             r = 0
             l = 0
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)    
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)    
         elif 0.1 >= x >= -0.1 and y <= -0.1: #full speed forwards
             x = abs(x)
             y = abs(y)
             r = 100 * y
             l = 100 * y
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
         elif 0.1 >= x >= -0.1 and y >= 0.1: #full speed backwards
             x = abs(x)
             y = abs(y)
             r = 100 * y
             l = 100 * y
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r) 
+            pz.setMotor(0,l)
+            pz.setMotor(1,r) 
         elif x <= -0.1 and 0.1 >= y >= -0.1: #spin right
             x = abs(x)
             y = abs(y)
             r = -100 * x
             l = 100 * x
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
         elif x >= 0.1 and 0.1 >= y >= -0.1: #spin left
             x = abs(x)
             y = abs(y)
             r = 100 * x
             l = -100 * x
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r) 
+            pz.setMotor(0,l)
+            pz.setMotor(1,r) 
         elif -0.9 < x < -0.1 and -0.9 < y < -0.1: #turnR - forwards
             x = abs(x)
             y = abs(y)
             r = 100 * x * (1-y)
             l = 100 * x
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
         elif 0.9 > x > 0.1 and -0.9 < y < -0.1: #turnL - forwards
             x = abs(x)
             y = abs(y)
             r = 100 * x
             l = 100 * x * (1-y)
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
         elif 0.9 > x > 0.1 and 0.1 > y > 0.1: #turnL - backwards
             x = abs(x)
             y = abs(y)
             r = -100 * x
             l = -100 * x * (1-y)
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
         elif x < -0.1 and y > 0.1: #turnR - backwards
             x = abs(x)
             y = abs(y)
             r = -100 * x * (1-y)
             l = -100 * x 
-            pz.setMotor(leftmotor,l)
-            pz.setMotor(rightmotor,r)
+            pz.setMotor(0,l)
+            pz.setMotor(1,r)
     
 
 def linefollow():
     print ("Line Following Program Active")
-    lcd_string("Line Following  <",LCD_LINE_1)
-    lcd_string("Select Ends     <",LCD_LINE_2)
+    mylcd.lcd_display_string("Line Follow", 1)
+    mylcd.lcd_display_string("Select Ends", 2)
     RUN = 1
     while RUN == 1:
         if buttons_pressed & 1 << SixAxis.BUTTON_SELECT:
@@ -201,8 +132,8 @@ def linefollow():
 
 def automaze():
     print("Automaze Program Active")
-    lcd_string("Automaze        <",LCD_LINE_1)
-    lcd_string("Select Ends     <",LCD_LINE_2)
+    mylcd.lcd_display_string("Automaze", 1)
+    mylcd.lcd_display_string("Select Ends", 2)
     LINE = 0 #set reflectivity - swap with BACKGROUND to invert
     BACKGROUND = 1 #set reflectivity - swap with LINE to invert
     RUN = 1
@@ -213,8 +144,8 @@ def automaze():
 
 def autospeed():
     print("Autospeed Program Active")
-    lcd_string("Autospeed       <",LCD_LINE_1)
-    lcd_string("Select Ends     <",LCD_LINE_2)
+    mylcd.lcd_display_string("Autospeed", 1)
+    mylcd.lcd_display_string("Select Ends", 2)
     RUN = 1
     while RUN == 1:
         if buttons_pressed & 1 << SixAxis.BUTTON_SELECT:
@@ -234,35 +165,31 @@ def autospeed():
 
 def wallstop():
     print("Wall Stop Program Active")
-    lcd_string("Wallstop        <",LCD_LINE_1)
-    lcd_string("Select Ends     <",LCD_LINE_2)
+    mylcd.lcd_display_string("Wall Stop", 1)
+    mylcd.lcd_display_string("Select Ends", 2)
+    
     RUN = 1
     while RUN == 1:
         if buttons_pressed & 1 << SixAxis.BUTTON_SELECT:
             RUN = 0
         elif RANGE > 10:
             pz.forward(100)
-            lcd_string("Range=          <",LCD_LINE_1)
-            lcd_string(RANGE,"     <",LCD_LINE_2)
+            mylcd.lcd_display_string("Range= %d %%" % RANGE, 2)
         elif 5 < RANGE <= 10:
             pz.forward(40)
-            lcd_string("Range=          <",LCD_LINE_1)
-            lcd_string(RANGE,"     <",LCD_LINE_2)
+            mylcd.lcd_display_string("Range= %d %%" % RANGE, 2)
         elif 2 < RANGE <= 5:
             pz.forward(20)
-            lcd_string("Range=          <",LCD_LINE_1)
-            lcd_string(RANGE,"     <",LCD_LINE_2)
+            mylcd.lcd_display_string("Range= %d %%" % RANGE, 2)
         elif 1 <= RANGE <= 2:
             pz.forward(10)
-            lcd_string("Range=          <",LCD_LINE_1)
-            lcd_string(RANGE,"     <",LCD_LINE_2)
+            mylcd.lcd_display_string("Range= %d %%" % RANGE, 2)
         elif RANGE < 1:
             pz.stop(0)
-            lcd_string("Range=          <",LCD_LINE_1)
-            lcd_string(RANGE,"     <",LCD_LINE_2)
+            mylcd.lcd_display_string("Range= %d %%" % RANGE, 2)
             time.sleep(2)
-            lcd_string("Wallstop Ended  <",LCD_LINE_1)
-            lcd_string("Select For Menu <",LCD_LINE_2)
+            mylcd.lcd_display_string("Ended", 1)
+            mylcd.lcd_display_string("Press Select", 2)
             time.sleep(10)
 
 
@@ -273,9 +200,8 @@ MAINRUN = 1
 #LCD prompt
 COUNTDOWN = 9
 while COUNTDOWN > 0:
-    DISPLAYCOUNTDOWN = "Connect PS3 - " , COUNTDOWN , "<"
-    lcd_string("Starting Up     <",LCD_LINE_1)
-    lcd_string(DISPLAYCOUNTDOWN ,LCD_LINE_2)
+    mylcd.lcd_display_string("Connect", 1)
+    mylcd.lcd_display_string("Controller %d %%" % COUNTDOWN, 2)
     time.sleep(1)
     COUNTDOWN = COUNTDOWN - 1
 
@@ -284,45 +210,42 @@ with SixAxisResource() as joystick:
     
     #Main loop - using this for the menu system
     while MAINRUN == 1:
-        lcd_string("Main Menu       <",LCD_LINE_1)
-        lcd_string("Select Program  <",LCD_LINE_2)
+        mylcd.lcd_display_string("Main Menu", 1)
+        mylcd.lcd_display_string("Select Program", 2)
 
         buttons_pressed = joystick.get_and_clear_button_press_history()
 
         pz.stop()
 
         if DEVELOPER == 1: #check for developer switch activation and if positive kill program
-            lcd_string("Killing Program <",LCD_LINE_1)
-            lcd_string("                <",LCD_LINE_2)
+            mylcd.lcd_display_string("Killing", 1)
+            mylcd.lcd_display_string("Program", 2)
             time.sleep(5)
             MAINRUN = 0
         elif buttons_pressed & 1 << SixAxis.BUTTON_START: #shutdown the pi if start is pressed
-            lcd_string("Shutting Down   <",LCD_LINE_1)
-            lcd_string("Confirm?        <",LCD_LINE_2)
+            mylcd.lcd_display_string("Shut Down", 1)
+            mylcd.lcd_display_string("Confirm?", 2)
             time.sleep(2)
             if buttons_pressed & 1 << SixAxis.BUTTON_START:
                 pz.cleanup()
                 os.system("shutdown now -h")
             else:
-                lcd_string("Shut Down       <",LCD_LINE_1)
-                lcd_string("Cancelled       <",LCD_LINE_2)
+                mylcd.lcd_display_string("Shut Down", 1)
+                mylcd.lcd_display_string("Cancelled", 2)
                 time.sleep(2)
-                lcd_string("Main Menu       <",LCD_LINE_1)
-                lcd_string("Select Program  <",LCD_LINE_2)
         elif buttons_pressed & 1 << SixAxis.BUTTON_SQUARE:
-            lcd_string("Starting.....   <",LCD_LINE_1)
-            lcd_string("Remote Control  <",LCD_LINE_2)
+            mylcd.lcd_display_string("Starting", 1)
+            mylcd.lcd_display_string("Remote Control", 2)
             time.sleep(2)            
             remotecontrol()
         elif buttons_pressed & 1 << SixAxis.BUTTON_CIRCLE:
             print ("Square Pressed")
-        elif buttons_pressed & 1 << SixAxis.BUTTON_TRIANGE:
+        elif buttons_pressed & 1 << SixAxis.BUTTON_TRIANGLE:
             print ("Square Pressed")
         elif buttons_pressed & 1 << SixAxis.BUTTON_CROSS:
             print ("Square Pressed")
 
-lcd_string("Program Dead    <",LCD_LINE_1)
-lcd_string("                <",LCD_LINE_2)
+mylcd.lcd_display_string("Ended", 1)
 time.sleep(2)
 pz.cleanup()
 
