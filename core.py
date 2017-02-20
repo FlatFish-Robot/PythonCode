d9521f4fc8f9316e96821274fe8002931856df72
Tim <tim.mockford@aceschools.net>
2017-02-20 21:46:09 +0000

    added screen

--- core.py
+++ core.py
@@ -28,8 +28,8 @@ mylcd.lcd_display_string("                ", 2)
 #pygame setup
 
 pygame.init()
-#screen = pygame.display.set_mode((100,100))
-#pygame.display.set_caption("Hello World")
+screen = pygame.display.set_mode((100,100))
+pygame.display.set_caption("Hello World")
 
 #____________________________________________________________________________________
 #hardware setup
