c140500903447c6e270ca118736a61b818e93d07
Tim <tim.mockford@aceschools.net>
2017-02-20 10:23:30 +0000

    removed imports

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 #core script to run at startup for flatfish robot
-from triangula.input import SixAxis, SixAxisResource
+#from triangula.input import SixAxis, SixAxisResource
 import time
-import smbus
-import os
-import hcsr04
+#import smbus
+#import os
+#import hcsr04
 import piconzero as pz
 #from gpiozero import Button
-import I2C_LCD_driver
+#import I2C_LCD_driver
 
 #__________________________________________________________________
 #initiate piconzero and display
