0280032bd327d22a44085ad9c2efaacad4ea40a3
Tim <tim.mockford@aceschools.net>
2017-02-20 10:21:56 +0000

    test script

---
+++ test.py
@@ -0,0 +1,17 @@
+#core script to run at startup for flatfish robot
+from triangula.input import SixAxis, SixAxisResource
+import time
+import smbus
+import os
+import hcsr04
+import piconzero as pz
+#from gpiozero import Button
+import I2C_LCD_driver
+
+#__________________________________________________________________
+#initiate piconzero and display
+
+pz.init() #initiate hardware
+pz.forward(100)
+time.sleep(5)
+pz.stop()
