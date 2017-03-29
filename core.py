6af3b05b8f59e672811f4b1ca9834bc406c89862
mockersACE <tim.mockford@aceschools.net>
2017-03-29 14:45:20 +0100

    changed timings

--- core.py
+++ core.py
@@ -139,14 +139,12 @@ def linefollower():
         RIGHTLINE = pz.readInput(2) #assign right line sensor to a variable
         LEFTLINE = pz.readInput(3) #assign left line sensor to a variable   
         if RIGHTLINE == 0:
-            pz.spinRight(70)
-            time.sleep(0.2)
+            pz.spinRight(60)
         elif LEFTLINE == 0:
-            pz.spinLeft(70)
-            time.sleep(0.2)
+            pz.spinLeft(60)
         else:
             pz.forward(50)
-            time.sleep(0.2)
+            time.sleep(0.5)
         #KEYS = True
         #while KEYS == True:
             #keys for escape
