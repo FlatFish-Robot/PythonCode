317f9706b97dc20df7a0d70a076efc31edeed03a
mockersACE <tim.mockford@aceschools.net>
2017-03-29 14:30:17 +0100

    removed sleeps

--- core.py
+++ core.py
@@ -140,12 +140,11 @@ def linefollower():
         LEFTLINE = pz.readInput(3) #assign left line sensor to a variable   
         if RIGHTLINE == 0:
             pz.spinRight(70)
-            time.sleep(0.3)
         elif LEFTLINE == 0:
             pz.spinLeft(70)
-            time.sleep(0.3)
         else:
             pz.forward(50)
+            
         #KEYS = True
         #while KEYS == True:
             #keys for escape
